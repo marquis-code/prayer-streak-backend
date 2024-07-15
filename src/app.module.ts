f1b6b50109c0f9c1dd7685d5d71b1a9ab06533e0
dotun2203 <adedotunabimbola2@gmail.com>
2024-07-15 13:44:27 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/auth-workshop'),
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.userModel.create(createUserDto);
+    return user.save();
+  }
+
+  async findAll() {
+    const users = this.userModel.find();
+    return users;
+  }
+
+  async findOne(id: number) {
+    const user = await this.userModel.findOne({ where: { id: id } });
+    if (user) {
+      return user;
+    }
+    throw new NotFoundException('user not found');
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true });
+  }
+
+  remove(id: number) {
+    return this.userModel.findByIdAndDelete(id);
+  }
+}
